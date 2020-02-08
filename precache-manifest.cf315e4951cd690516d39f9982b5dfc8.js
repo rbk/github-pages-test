7553b37ce7979bb8e660a0229e83028cd47b1787
Richard Keller <richard.be.jamin@gmail.com>
2020-02-08 08:49:04 -0600

    Updates

---
+++ precache-manifest.cf315e4951cd690516d39f9982b5dfc8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ee85cdd09769ac494b169f5b79bde6a",
+    "url": "/github-pages-test/index.html"
+  },
+  {
+    "revision": "84e0a6be95aa5bf07194",
+    "url": "/github-pages-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "818e4ceefdfb5532773b",
+    "url": "/github-pages-test/static/js/2.bd29c024.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/github-pages-test/static/js/2.bd29c024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e0a6be95aa5bf07194",
+    "url": "/github-pages-test/static/js/main.df91c191.chunk.js"
+  },
+  {
+    "revision": "726589fbbb75ab43b7b4",
+    "url": "/github-pages-test/static/js/runtime-main.41016f37.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-pages-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
